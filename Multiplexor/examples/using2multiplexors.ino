234e2bab285a398a9b9d49e28593807902ed3bc0
CrhistianDLM <cristian.lucumi00@usc.edu.co>
2015-12-01 07:46:58 -0500

    Update using2multiplexors.ino

--- Multiplexor/examples/using2multiplexors.ino
+++ Multiplexor/examples/using2multiplexors.ino
@@ -2,24 +2,23 @@
 Multiplexor m;
 void setup() {
   // put your setup code here, to run once:
-  //pin latch 8
-  //pin clock 11
-  //pin data 12
+  //latch 8
+  //clock 11
+  //data 12
   m.begin(8, 11, 12);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   //Mostrara una secuencia en un multiplexor y los dos primeros en el otro
-  //Display a sequence in a multiplexer and the first two on the other
   for(int i = 0; i<9; i++){
-    m.writeLeds2(i,3);
+    m.writeLeds_m1(i);
     delay(200);
   }
 
   //Mostrara una secuencia en un multiplexor y el penultimo led en el otro
-  //Display a sequence in a multiplexer and the penultimate led in the other
   for(int i = 8; i>0; i--){
-    m.writeLeds2(i,64);
+    m.writeLeds_m2(i);
     delay(200);
   }
 }
